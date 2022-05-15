9c9ba4d29136d1db2ace1d9460beeb25f747d65e
aadityas201 <69757025+aadityas201@users.noreply.github.com>
2022-05-15 21:55:10 +0530

    Cart UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 // import Home from "./pages/Home";
 // import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
+// import Cart from "./pages/Cart";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
+      {/* <ProductList /> */}
+      {/* <Cart /> */}
+      <Product />
     </div>
   );
 }

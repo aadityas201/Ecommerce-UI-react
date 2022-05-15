87b8f56c69cfdbd0291c4076440a0f840084b2c8
aadityas201 <69757025+aadityas201@users.noreply.github.com>
2022-05-15 18:08:27 +0530

    HOME UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import MainSlider from "../components/MainSlider";
+import Categories from "../components/Categories";
+
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <MainSlider />
+      <Categories />
     </div>
   );
 };
